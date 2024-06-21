40be444182815c54e6db05711db5657f2fdca215
Sandra <sandra.gufler@gmx.ch>
2024-06-21 12:14:38 +0200

    feat: connect to strapi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+async function getStrapiData(path: string) {
+  const baseUrl = 'http://localhost:1337';
+
+  try {
+    const response = await fetch(baseUrl + path);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const cmsData = await getStrapiData('/api/home-page');
+
+  const { title, description } = cmsData.data.attributes;
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button variant='outline'>hello</Button>
+    <main className='container py-6 mx-auto'>
+      <Button variant='outline'>{title}</Button>
+      <Button variant='outline'>{description}</Button>
     </main>
   );
 }
